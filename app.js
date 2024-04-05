1af988628da74524827a43b3d8dfab892ad4c0a2
seojin7022 <seojin7022@gmail.com>
2024-04-06 08:22:55 +0900

    Commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const startButton = document.querySelector('.start-button');
 const chapter = document.querySelector('#Chapter');
+const mode = document.querySelector('#Mode');
 
 startButton.addEventListener('click', () => {
     console.log("Start");
     window.localStorage.setItem('Chapter', chapter.value);
+    window.localStorage.setItem('Mode', mode.value);
     window.location.href = './exam.html';
 });
 
--- exam.js
+++ exam.js
@@ -1,6 +1,10 @@
 const title = document.querySelector('title');
 const chapter =  window.localStorage.getItem('Chapter');
-title.innerText = chapter + "단원";
+const mode =  window.localStorage.getItem('Mode');
+const topTitle = document.querySelector('#app h1');
+const backButton = document.querySelector('.back');
+title.innerText = chapter + "단원 " + mode;
+topTitle.innerText = chapter + "단원 " + mode;
 
 let data = {
     "1": {
@@ -9,25 +13,86 @@ let data = {
     }
 }
 
+let voca = {
+    "1": {
+
+        "廣": "넓을 광",
+        "範": "법 범",
+        "圍": "에워쌀 위",
+        "資": "재물 자",
+        "料": "헤아릴 료(요)"
+
+    }
+}
+
 
 data = data[chapter];
+voca = voca[chapter]
 
 const quizQuestion = document.querySelector('.quiz-question');
 const quizAnswer = document.querySelector('.quiz-answer');
+const quizSelect = document.querySelector('.quiz-select');
+const quizSelectButtons = document.querySelectorAll('.quiz-select button');
 
-const onAnswered = () => {
-    quizAnswer.value = "";
-    const key = Object.keys(data)[Math.floor(Math.random() * Object.keys(data).length)];
-    quizQuestion.innerText = key;
-    quizAnswer.innerText = data[key];
+if (mode === "어휘" || mode === "객관식") {
+    quizSelect.classList.remove("hide");
+
+    if (mode === "객관식")
+    {
+        voca = data;
+    }
+    let key = "";
+
+    const onAnswered = () => {
+        key = Object.keys(voca)[Math.floor(Math.random() * Object.keys(voca).length)];
+        quizQuestion.innerText = key;
+        let answers = [voca[key]];
+        while (answers.length < 4) {
+            const randomKey = Object.keys(voca)[Math.floor(Math.random() * Object.keys(voca).length)];
+            const randomValue = voca[randomKey];
+            if (!answers.includes(randomValue)) {
+                answers.push(randomValue);
+            }
+        }
+        answers.sort(() => Math.random() - 0.5);
+        for (let i = 0; i < 4; i++) {
+            quizSelectButtons[i].innerText = answers[i];            
+        }
+    }
+
+    quizSelectButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            if (button.innerText === voca[key] && event.target == button) {
+                onAnswered();
+            }
+        })
+    })
+
+    onAnswered();
+}
+else if (mode === "주관식") {
+    quizAnswer.classList.remove("hide");
+
+    const onAnswered = () => {
+        quizAnswer.value = "";
+        const key = Object.keys(data)[Math.floor(Math.random() * Object.keys(data).length)];
+        quizQuestion.innerText = key;
+        quizAnswer.innerText = data[key];
+    }
+    
+    quizAnswer.addEventListener('keydown', (event) => {
+        if (event.key === "Enter" && quizAnswer.value === data[quizQuestion.innerText]) {
+            onAnswered();
+        }
+    })
+    
+    
+    onAnswered();
 }
 
-quizAnswer.addEventListener('change', () => {
-    if (quizAnswer.value === data[quizQuestion.innerText]) {
-        onAnswered();
-    }
+
+backButton.addEventListener('click', () => {
+    window.location.href = "index.html";
 })
 
-
-onAnswered();
 
